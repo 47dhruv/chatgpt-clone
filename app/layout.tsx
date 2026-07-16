c0365f2e1c95b269af064ef9423e767c094167ef
47dhruv <dhruvpandey744@gmail.com>
2026-07-16 12:51:08 +0530

    feat: add Prisma setup with PostgreSQL integration
    - Updated package.json to include Prisma dependencies: @prisma/adapter-pg, @prisma/client, and prisma.
    - Created prisma.config.ts for Prisma configuration.
    - Added initial migration to create a "Test" table in the database.
    - Created migration lock file for version control.
    - Defined Prisma schema in schema.prisma with a "Test" model.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
+import { Tooltip } from "@/components/ui/tooltip";
 
 const playfairDisplay = Playfair_Display({subsets:['latin'],variable:'--font-serif'});
 
@@ -29,8 +32,23 @@ export default function RootLayout({
     <html
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-serif", playfairDisplay.variable)}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <QueryProvider>
+
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            >
+
+        {children}
+        </ThemeProvider>
+            </QueryProvider>
+        </body>
     </html>
   );
 }
+
